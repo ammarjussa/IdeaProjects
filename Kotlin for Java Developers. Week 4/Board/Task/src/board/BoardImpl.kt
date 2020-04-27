869b123f05651a07ad9301feb2d9eb8c3a15db87
Ammar <ammar26497@gmail.com>
2020-04-27 16:50:32 +0500

    Square Board Done

--- Kotlin for Java Developers. Week 4/Board/Task/src/board/BoardImpl.kt
+++ Kotlin for Java Developers. Week 4/Board/Task/src/board/BoardImpl.kt
@@ -1,7 +1,137 @@
 package board
 
 import board.Direction.*
+import java.lang.IllegalArgumentException
 
-fun createSquareBoard(width: Int): SquareBoard = TODO()
+fun createSquareBoard(width: Int): SquareBoard = MySquareBoard(width)
 fun <T> createGameBoard(width: Int): GameBoard<T> = TODO()
+
+class MySquareBoard(override val width: Int): SquareBoard {
+    private var cellBoard = mutableListOf<List<Cell>>()
+    init {
+        for(i in 1 .. width) {
+            var cells = mutableListOf<Cell>()
+            for(j in 1 .. width) {
+                cells.add(Cell(i,j))
+            }
+            cellBoard.add(cells)
+         }
+    }
+
+    override fun getCellOrNull(i: Int, j:Int ): Cell? {
+        if(i in 1..width && j in 1..width) {
+            return cellBoard[i-1][j-1]
+        }
+        return null
+    }
+
+    override fun getCell(i: Int, j: Int): Cell {
+        if(i in 1..width && j in 1..width) {
+            return cellBoard[i-1][j-1]
+        }
+
+        throw IllegalArgumentException("Not Such cell")
+    }
+
+    override fun getAllCells(): Collection<Cell> {
+        var ans = mutableListOf<Cell>()
+        for(cells in cellBoard) {
+            for(cell in cells) {
+                ans.add(cell)
+            }
+        }
+
+        return ans
+    }
+
+    override fun getRow(i: Int, jRange: IntProgression): List<Cell> {
+        var ans = mutableListOf<Cell>()
+        var start = jRange.first
+        var end = 0
+        end = if(jRange.last > width) {
+            width
+        }
+
+        else {
+            jRange.last
+        }
+
+        var reversed = false
+        if(start > end) {
+            reversed = true
+            var temp = start
+            start=end
+            end=temp
+        }
+
+
+        for(k in start..end) {
+            ans.add(cellBoard[i-1][k-1])
+        }
+
+        return if (reversed) ans.reversed() else ans
+    }
+
+    override fun getColumn(iRange: IntProgression, j: Int): List<Cell> {
+        var ans = mutableListOf<Cell>()
+        var start = iRange.first
+        var end = 0
+        end = if(iRange.last > width) {
+            width
+        }
+
+        else {
+            iRange.last
+        }
+
+        var reversed = false
+        if(start > end) {
+            reversed = true
+            var temp = start
+            start=end
+            end=temp
+        }
+
+        for(k in start..end) {
+            ans.add(cellBoard[k-1][j-1])
+        }
+
+        return if (reversed) ans.reversed() else ans
+    }
+
+    override fun Cell.getNeighbour(direction: Direction): Cell? {
+        var ith = this.i
+        var jth = this.j
+
+        when(direction) {
+            UP -> ith -= 1
+            DOWN -> ith+=1
+            LEFT -> jth-=1
+            RIGHT -> jth+=1
+        }
+
+        if(ith in 1..width && jth in 1..width) return cellBoard[ith-1][jth-1]
+        return null
+    }
+}
+
+//open class MyGameBoard<T>(override val width: Int) : GameBoard<T> {
+//
+//    override fun filter(predicate: (T?) -> Boolean): Collection<Cell> {
+//        TODO("Not yet implemented")
+//    }
+//
+//    override fun find(predicate: (T?) -> Boolean): Cell? {
+//        TODO("Not yet implemented")
+//    }
+//
+//    override fun any(predicate: (T?) -> Boolean): Boolean {
+//        TODO("Not yet implemented")
+//    }
+//
+//    override fun all(predicate: (T?) -> Boolean): Boolean {
+//        TODO("Not yet implemented")
+//    }
+//
+//}
 
--- Kotlin for Java Developers. Week 4/Rationals/Task/src/rationals/Rational.kt
+++ Kotlin for Java Developers. Week 4/Rationals/Task/src/rationals/Rational.kt
@@ -1,8 +1,170 @@
 package rationals
 
-class Rational {
+import java.lang.IllegalArgumentException
+import java.math.BigInteger
+import java.math.BigInteger.ZERO
+
+data class Rational(val num: BigInteger, val den: BigInteger): Comparable<Rational> {
+    private var numerator : BigInteger = num
+    private var denominator: BigInteger = den
+
+    init {
+        if(den == "0".toBigInteger()) {
+            throw IllegalArgumentException("Denominator cannot be zero")
+        }
+        val gcd = numerator.abs().gcd(denominator.abs())
+
+        val sign = numerator.signum() * denominator.signum()
+
+        numerator = (numerator.abs() / gcd) * sign.toBigInteger()
+        denominator = denominator.abs() / gcd
+    }
+
+    operator fun plus(adder: Rational): Rational {
+        lateinit var nu: BigInteger
+        lateinit var de: BigInteger
+
+
+        if(this.denominator == adder.denominator) {
+            nu=this.numerator.plus(adder.numerator)
+            de=this.denominator
+            return Rational(nu,de)
+
+        }
+
+        else {
+            val thegcd: BigInteger = this.denominator.gcd(adder.denominator)
+
+            de = (this.denominator.times(adder.denominator))
+                    .divide(thegcd)
+            val adding=this.numerator.times(adder.denominator.divide(thegcd))
+            val adding2=adder.numerator.times(this.denominator.divide(thegcd))
+            nu=adding.plus(adding2)
+
+            return Rational(nu, de)
+        }
+    }
+
+    operator fun minus(subt : Rational): Rational {
+        lateinit var nu: BigInteger
+        lateinit var de: BigInteger
+        if(this.denominator == subt.denominator) {
+            nu=this.numerator.minus(subt.numerator)
+            de=this.denominator
+            return Rational(nu,de)
+        }
+
+        else {
+            val thegcd: BigInteger = this.denominator.gcd(subt.denominator)
+
+            de = (this.denominator.times(subt.denominator))
+                    .divide(thegcd)
+
+            val sub1=this.numerator.times(subt.denominator.divide(thegcd))
+            val sub2=subt.numerator.times(this.denominator.divide(thegcd))
+            nu=sub1.minus(sub2)
+
+            return Rational(nu,de)
+        }
+    }
+
+    operator fun times(mult: Rational): Rational {
+        var nu: BigInteger = this.numerator.times(mult.numerator)
+        var de: BigInteger = this.denominator.times(mult.denominator)
+
+        return Rational(nu,de)
+    }
+
+    operator fun div(div : Rational): Rational {
+        var temp: BigInteger = div.numerator
+        div.numerator=div.denominator
+        div.denominator=temp
+
+        return this.times(div)
+    }
+
+    operator fun unaryMinus(): Rational {
+        var nu = -this.numerator
+        var de = this.denominator
+        return Rational(nu,de)
+    }
+
+    override fun compareTo(other: Rational): Int {
+        val lhs = this.numerator * other.denominator
+        val rhs = this.denominator * other.numerator
+        return when {
+            lhs < rhs -> -1
+            lhs > rhs -> 1
+            else -> 0
+        }
+    }
+
+
+    override fun toString(): String {
+        return if(this.denominator == "1".toBigInteger()) {
+            this.numerator.toString()
+        } else {
+            var num = this.numerator
+            var dem = this.denominator
+
+            val gcd = num.gcd(dem)
+            if(dem < ZERO) {
+                "${(-num/gcd).toString()}/${(-dem/gcd).toString()}"
+            }
+
+
+            else "${(num/gcd).toString()}/${(dem/gcd).toString()}"
+        }
+    }
+
+
+    override fun hashCode(): Int {
+        return this.toString().hashCode()
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (other == null)
+            return false
+
+        if (other !is Rational)
+            return false
+
+        return (this.numerator == other.numerator) &&
+                (this.denominator == other.denominator)
+    }
+
+    operator fun rangeTo(end: Rational): ClosedRange<Rational> {
+        return object : ClosedRange<Rational> {
+            override val endInclusive: Rational = end
+            override val start: Rational = this@Rational
+        }
+    }
 
 }
+
+
+infix fun Int.divBy(divisor: Int) : Rational {
+    return Rational(this.toBigInteger(), divisor.toBigInteger())
+}
+
+infix fun Long.divBy(divisor: Long) : Rational {
+    return Rational(this.toBigInteger(), divisor.toBigInteger())
+}
+
+infix fun BigInteger.divBy(divisor: BigInteger) : Rational {
+    return Rational(this, divisor)
+}
+
+fun String.toRational() : Rational {
+    val str = this.split('/')
+
+    if(str.size == 1) {
+        return Rational(str[0].toBigInteger(), "1".toBigInteger())
+    }
+    return Rational(str[0].toBigInteger(), str[1].toBigInteger())
+}
+
+
 
 
 fun main() {
@@ -31,12 +193,24 @@ fun main() {
     val twoThirds = 2 divBy 3
     println(half < twoThirds)
 
+
     println(half in third..twoThirds)
 
+    //Left
     println(2000000000L divBy 4000000000L == 1 divBy 2)
 
+    //Left
     println("912016490186296920119201192141970416029".toBigInteger() divBy
             "1824032980372593840238402384283940832058".toBigInteger() == 1 divBy 2)
 }
+
+private operator fun Rational.contains(half: Rational): Boolean {
+    if(this < half) return true
+    return false
+}
+
+
+
+
 
 
